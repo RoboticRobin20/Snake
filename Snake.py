851de589559e1d942273131be21f614d5b81c6b2
RoboticRobin20 <thoelro@cronos.be>
2026-01-09 17:33:39 +0100

    new class main and added movement to snake

--- Snake.py
+++ Snake.py
@@ -14,6 +14,11 @@ class Fruit:
         fruit_rect = pygame.Rect(int(self.pos.x * cellsize), int(self.pos.y * cellsize), cellsize, cellsize)
         pygame.draw.rect(screen, (126, 166, 114), fruit_rect)
 
+    def randomize_fruit(self):
+        self.x = random.randint(0, cellnumber - 1)
+        self.y = random.randint(0, cellnumber - 1)
+        self.pos = Vector2(self.x, self.y)
+
 
 class Snake:
     def __init__(self):
@@ -32,6 +37,26 @@ class Snake:
         body_copy.insert(0, self.body[0] + self.direction)
         self.body = body_copy
 
+    def change_direction(self, direction):
+        if self.direction.x != direction.x and self.direction.y != direction.y:
+            self.direction = direction
+
+    def grow(self):
+        self.body.insert(len(self.body) - 1, self.body[-1])
+
+
+class Main:
+    def __init__(self):
+        self.fruit = Fruit()
+        self.snake = Snake()
+    
+    def update(self):
+        self.snake.move_snake()
+
+    def draw_elements(self):
+        self.fruit.draw_fruit()
+        self.snake.draw_snake()
+
 # pygame setup
 pygame.init()
 cellsize = 40
@@ -41,8 +66,11 @@ screen = pygame.display.set_mode((cellsize * cellnumber, cellsize * cellnumber))
 clock = pygame.time.Clock()
 running = True
 
-fruit = Fruit()
-snake = Snake()
+
+SCREEN_UPDATE = pygame.USEREVENT
+pygame.time.set_timer(SCREEN_UPDATE, 150)
+
+main_game = Main()
 
 while running:
     # poll for events
@@ -50,13 +78,23 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == SCREEN_UPDATE:
+            main_game.update()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_DOWN:
+                main_game.snake.change_direction(Vector2(0,1))
+            elif event.key == pygame.K_UP:
+                main_game.snake.change_direction(Vector2(0,-1))
+            elif event.key == pygame.K_LEFT:
+                main_game.snake.change_direction(Vector2(-1,0))
+            elif event.key == pygame.K_RIGHT:
+                main_game.snake.change_direction(Vector2(1,0))
 
     # fill the screen with a color to wipe away anything from last frame
     screen.fill((175, 215,70))
-    fruit.draw_fruit()
-    snake.draw_snake()
 
     # RENDER YOUR GAME HERE
+    main_game.draw_elements()
     pygame.display.update()
     clock.tick(60)  # limits FPS to 60
 
